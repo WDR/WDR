53d64d9d1e7ff371eda84258c66d61b13a85cc03
Marcin Płonka <mplonka@gmail.com>
2014-04-22 12:39:32 +0200

    enhancement: update date ranges in copyright headers

--- lib/common/wdr/app.py
+++ lib/common/wdr/app.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2012,2013 Marcin Plonka <mplonka@gmail.com>
+# Copyright 2012,2014 Marcin Plonka <mplonka@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
