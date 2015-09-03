4d4c1fd2265e099623ac9bdcaa2043ac80c51638
Marcin Płonka <mplonka@gmail.com>
2015-09-03 07:26:22 +0200

    bumped up version number

--- lib/common/wdr/__init__.py
+++ lib/common/wdr/__init__.py
@@ -6,8 +6,8 @@ __all__ = ['config', 'control', 'task', 'manifest', 'util']
 logger = logging.getLogger('wdr')
 
 MAJOR_VERSION = 0
-MINOR_VERSION = 5
-PATCH_VERSION = 1
+MINOR_VERSION = 6
+PATCH_VERSION = 0
 
 
 class WsadminObjects:
